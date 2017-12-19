164aa96e08e3c7eb8bf1aacd5523ef0b8c79c568
admin <admin@vraney-mbp.thelamplighterschool.org>
2017-12-18 18:31:29 -0600

    search box

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,14 @@
 // SETUP VARIABLES
 // ==========================================================
 
-
 var weatherAddress = "http://api.openweathermap.org/data/2.5/forecast?mode=json&";
 var ticketQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Woz0yAkff0NLy3N8cRrg7O7nd3x7qr8l&sort=date,asc"
 var venueQueryURL = "https://app.ticketmaster.com/discovery/v2/venues.json?apikey=Woz0yAkff0NLy3N8cRrg7O7nd3x7qr8l&sort=distance,asc"
-
 var scheduleQueryURL = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/full_game_schedule.json?date=since-yesterday"
 
-var playerQueryURL04 = "http://api.fantasy.nfl.com/players/stats?statType=seasonStats&season=2017&format=json"
-var playerQueryURL = "http://api.fantasy.nfl.com/v1/players/researchinfo?count=9999&format=json"
+
+// var playerQueryURL04 = "http://api.fantasy.nfl.com/players/stats?statType=seasonStats&season=2017&format=json"
+
 
 var gameMoment;
 var gameLocation;
@@ -25,8 +24,282 @@ var marker;
 // FUNCTIONS
 // ==========================================================
 
-//Google Map API
 
+
+
+
+
+$(document).ready(function() {
+  $("#searchPanel").hide();
+  $("#resultsBox").hide();
+
+  $("#submit-Button").on("click", function(event) {
+    event.preventDefault();
+
+    $("#searchPanel").show();
+
+    //clears table with list of names from previous search
+    $("#tableSearchList").empty();
+    $("#playerNewsContainer").empty();
+
+    
+    playerseachMSF();
+    // playerSearchESPN();
+  })
+})
+
+
+
+// CORS API - does not currently work
+function playerSearch() {
+  var queryURL = "https://api.sportradar.us/nfl-ot2/players/0acdcd3b-5442-4311-a139-ae7c506faf88/profile.xml?api_key=y8k4vea62ypyg6us7vjp4nhm"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(respone) {
+    console.log(queryURL)
+  })
+}
+
+
+
+
+function playerSearchESPN() {
+  var queryURL = "http://api.fantasy.nfl.com/v1/players/researchinfo?count=9999&format=json";
+  var searchPlayerFN = $("#searchFN").val().trim(); 
+  var searchPlayerLN = $("#searchLN").val().trim();
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(respone) {
+
+    console.log("PlayerSearch ESPN - " + queryURL);
+
+///// IF searchPlayerFN is empty
+    var players = respone.players;
+    if (searchPlayerFN == 0) {
+      for (var i = 0; i < players.length; i++) {
+        if (players[i].lastName === searchPlayerLN) {
+          var playerResults_LN = players[i].lastName;
+          var playerResults_FN = players[i].firstName;
+          var playerResults_ID = players[i].id;
+          var playerResults_Abbr = players[i].teamAbr;
+          var playerResults_Position = players[i].position;
+          
+          playerInfo(playerResults_FN, playerResults_LN);
+        } 
+      }
+    } 
+    else {// IF searchPlayerFN has content
+      for (var i = 0; i < respone.players.length; i++) {
+        if (respone.players[i].lastName === searchPlayerLN && respone.players[i].firstName === searchPlayerFN) {
+          var playerResults_LN = respone.players[i].lastName;
+          var playerResults_FN = respone.players[i].firstName;
+
+          playerInfo(playerResults_FN, playerResults_LN);
+        }
+      }
+    }
+
+    function playerInfo(FN, LN) {
+      
+    }
+
+
+  })
+}
+
+
+
+
+
+//Player Seach API
+function playerseachMSF() {
+  var searchPlayerFN = $("#searchFN").val().trim(); 
+  var searchPlayerLN = $("#searchLN").val().trim();
+  var searchedPlayer = "";
+
+  //allows user to only search for last name and have fist name be optional
+  if (searchPlayerFN == 0) {
+    var searchedPlayer = searchPlayerLN;
+  } else {
+    var searchedPlayer = searchPlayerFN + "-" + searchPlayerLN;
+  }
+
+  var playerQueryURL = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/active_players.json?" + "player=" + searchedPlayer;
+  
+
+  $.ajax({
+    type: "GET",
+    url: playerQueryURL,
+    dataType: 'json',
+    async: true,
+    headers: {
+    "Authorization": "Basic " + btoa("chen" + ":" + "testing")
+  },
+  }).done(function(response) {
+    console.log("PlayerSearch MSF - " + playerQueryURL);
+
+    var playerResults = response.activeplayers.playerentry;
+
+    for (var i = 0; i < playerResults.length; i++) {
+
+      //removes any variables with undifined results
+      if (playerResults[i].team !== undefined) {
+        var playerResults_FN = playerResults[i].player.FirstName;
+        var playerResults_LN = playerResults[i].player.LastName;
+        var playerResults_Position = playerResults[i].player.Position;
+        var playerResults_Team = playerResults[i].team.Name;
+        var playerResults_City = playerResults[i].team.City;
+        var playerResults_CityAbbr = playerResults[i].team.Abbreviation;
+        var playerResults_IMG = playerResults[i].player.officialImageSrc;
+      }
+      if (playerResults[i].player.externalMapping !== null) {
+        var playerResults_ID = playerResults[i].player.externalMapping.ID;
+      }
+
+      //adds players that match searched name to table
+      $("#tableSearchList")
+        .append($("<tr>")
+          .attr("id", playerResults_ID)
+          .attr("data-FN", playerResults_FN)
+          .attr("data-LN", playerResults_LN)
+          .attr("class", "players")
+          .append( $("<td>").text(playerResults_FN + " " + playerResults_LN))
+          .append( $("<td>").text(playerResults_Team))
+          .append( $("<td>").text(playerResults_Position))
+        ) //<tr> append
+    } //for loop
+
+
+    // on click for when a player is selected
+    $(".players").on("click", function() {
+      $("#resultsBox").show();
+      $("#searchPanel").hide(1000);
+
+
+
+      var playerID = $(this).attr("id");
+      var playerFN = $(this).attr("data-FN");
+      var playerLN = $(this).attr("data-LN");
+
+      arrestRecord(playerFN, playerLN);
+      playerDisplay(playerID);
+      displayPlayerStats(playerFN, playerLN);
+      playerFantasyStats(playerFN, playerLN)
+    })
+
+
+
+
+  }) //done 
+}
+
+function playerFantasyStats(firstName, lastName) {
+  var queryURL = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/cumulative_player_stats.json?" + "player=" + firstName + "-" + lastName;
+  var queryURL02 = "https://api.mysportsfeeds.com/v1.0/pull/nfl/2017-regular/cumulative_player_stats.json?playerstats=Att,Comp,Yds,TD&" + "player=" + firstName + "-" + lastName;
+
+    $.ajax({
+    type: "GET",
+    url: queryURL02,
+    dataType: 'json',
+    async: true,
+    headers: {
+    "Authorization": "Basic " + btoa("chen" + ":" + "testing")
+  },
+  }).done(function(response) {
+    // console.log("Player Stats - " + queryURL02);
+
+    // var playerJSON = JSON.stringify(response.cumulativeplayerstats.playerstatsentry[0].stats[0]);
+
+
+    // console.log(response.cumulativeplayerstats.playerstatsentry[0].stats[0]);
+
+
+    // for (var i = 0; i < playerJSON.length; i++) {
+      
+    // }
+  })
+}
+
+
+
+function displayPlayerStats(firstName, lastName) {
+  var playerQueryURL = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/active_players.json?" + "player=" + firstName + "-" + lastName;
+  
+
+  $.ajax({
+    type: "GET",
+    url: playerQueryURL,
+    dataType: 'json',
+    async: true,
+    headers: {
+    "Authorization": "Basic " + btoa("chen" + ":" + "testing")
+  },
+  }).done(function(response) {
+    console.log("Player Stats - " + playerQueryURL);
+
+    var playerResults = response.activeplayers.playerentry[0];
+    var player_Num = playerResults.player.JerseyNumber;
+    var player_Position = playerResults.player.Position;
+    var player_Height = playerResults.player.Height;
+    var player_Weight = playerResults.player.Weight;
+    var player_Age = playerResults.player.Age;
+    var player_IMG = playerResults.player.officialImageSrc;
+    var player_TeamCity = playerResults.team.City;
+    var player_TeamName = playerResults.team.Name;
+
+    $("#headshot").attr("src", player_IMG);
+    $("#playerName").text(firstName + " " + lastName);
+    $("#playerTeam").text(player_TeamCity + " " + player_TeamName);
+    $("#playerPosition").text("Position " + player_Position);
+    $("#playerNum").text("#" + player_Num);
+    $("#playerHeight").text("Height " + player_Height);
+    $("#playerWeight").text("Weight " + player_Weight);
+    $("#playerAge").text(player_Age + " Years Old");
+
+
+  })
+
+}
+
+function playerDisplay(playerID) {
+  var queryURL = "http://api.fantasy.nfl.com/v1/players/details?playerId=" + playerID + "&statType=seasonStatsformat=json";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log("PlayerDisplay ESPN - " + queryURL);
+
+    var playerTeamAbbr = response.players[0].teamAbbr
+    searchAddress(playerTeamAbbr);
+    gameScheduleQuery(playerTeamAbbr);
+
+    
+
+    for (var i = 0; i < response.players[0].notes.length; i++) {
+      var playerNews = response.players[0].notes;
+      var playerNewsBody = playerNews[i].body;
+      var playerAnalysis = playerNews[i].analysis;
+      
+
+
+
+      $("#playerNewsContainer").append("<div>" + playerNewsBody + "</div>");
+      $("#playerNewsContainer").append("<br>");
+      $("#playerNewsContainer").append("<div>" + playerAnalysis + "</div>");
+      $("#playerNewsContainer").append("<div>" + "-------------------------------------------------" + "</div>");
+      $("#playerNewsContainer").append("<div>" + "-------------------------------------------------" + "</div>");
+    }
+
+
+  })
+}
+
+
+//Google Map API
 function searchAddress(addressInput) {
 
   var geocoder = new google.maps.Geocoder();
@@ -34,8 +307,8 @@ function searchAddress(addressInput) {
   geocoder.geocode({address: addressInput}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var myResult = results[0].geometry.location;
-      console.log(myResult.lat());
-      console.log(myResult.lng());
+      // console.log(myResult.lat());
+      // console.log(myResult.lng());
       searchWeather(myResult.lat(), myResult.lng());
       var latlng = (myResult.lat()+","+myResult.lng());
       venueSearch(latlng);
@@ -70,23 +343,6 @@ function initMap() {
   });
 }
 // --- Google Map API
-
-//Player Search API
-function playerSearch(name) {
-  var nameArray = name.split(" ");
-  var queryURL = playerQueryURL + nameArray[1] + "/" + nameArray[0];
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).done(function(response) {
-    console.log(response);
-    playerName = response[0].fullName;
-    console.log(playerName);
-    playerTeam = response[0].team;
-  });
-
-}
 
 // Team schedule search
 function gameScheduleQuery(team) {
@@ -131,7 +387,8 @@ function gameScheduleQuery(team) {
       
   });
 
-}
+};
+
 //
 
 //Open Weather API
@@ -142,6 +399,8 @@ function searchWeather(lat, lon){
     url: queryURL,
     method: "GET"
   }).done(function(response) {
+    console.log("Weather - " + queryURL);
+
     var index = 0;
     for (var i = 0; i < response.list.length - 1; i++) {
       if(response.list[i].dt_txt < gameMoment && response.list[i+1].dt_txt > gameMoment) {
@@ -150,19 +409,18 @@ function searchWeather(lat, lon){
       }
     }
     var weather = response.list[index];
-    console.log(weather);
     var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png");
     var city = $("<strong>").text(response.city.name);
     city.append(icon);
     var div = $("<div>").text();
     //div.append(icon);
-    $("#weather").html("<h4>Gameday Weather </h4>");
+    $("#weather").html("<h4>GameDay Weather: </h4>");
     $("#weather").append(city);
-    $("#weather").append("<div>Temperature: " + Math.round(weather.main.temp) + "°F </div>");
-    $("#weather").append("<div>Wind: " + weather.wind.speed + "&nbspmph</div>");
-    $("#weather").append("<div>Humidity: " + weather.main.humidity + "%</div><br>");
+    $("#weather").append("<div>Feels like&nbsp&nbsp&nbsp&nbsp" + Math.round(weather.main.temp) + "°F </div>");
+    $("#weather").append("<div>Wind&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + weather.wind.speed + "&nbspmph</div>");
+    $("#weather").append("<div>Humidity&nbsp&nbsp&nbsp" + weather.main.humidity + "%</div><br>");
 
-    console.log(weather.weather[0].description);
+    // console.log(weather.weather[0].description);
   })
 }
 // --- Open Weather API
@@ -170,25 +428,25 @@ function searchWeather(lat, lon){
 // ticketMaster
 function searchTicket() {
   var time = moment(gameMoment, "YYYY-MM-DD HH:mm").format("YYYY-MM-DDTHH:mm:ss[Z]");
-  console.log(time);
+  // console.log(time);
   var queryURL = ticketQueryURL + "&startDateTime=" + time + "&keyword=" + home;
-  console.log(queryURL);
+  
   $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function(response) {
-    console.log(response._embedded.events[0].name);
+    console.log("Ticket - " + queryURL);
+    // console.log(response._embedded.events[0].name);
     //console.log(response._embedded.events[0].url);
     //console.log(response._embedded.events[0].priceRanges[0].min);
     //console.log(response._embedded.events[0].priceRanges[0].max);
     var event = response._embedded.events[0];
-    $("#ticket").html("<h4>TicketMaster </h4>");
+    $("#ticket").html("<h4>TicketMaster: </h4>");
     var link = $("<a>").attr("href", event.url).attr("target", "_blank").text("Buy Tickets");
     $("#ticket").append(link);
 
-    var price = $("<div>").text("Price: $" + event.priceRanges[0].min + " to $" + event.priceRanges[0].max);
+    var price = $("<div>").text("Price Ranges: " + event.priceRanges[0].min + " USD to " + event.priceRanges[0].max + " USD");
     $("#ticket").append(price);
-
   });
 }
 function venueSearch(latlong) {
@@ -198,13 +456,14 @@ function venueSearch(latlong) {
     url: queryURL,
     method: "GET"
   }).done(function(response) {
-    //console.log(response);
+    console.log("Venue Search - " + queryURL);
     var venueId = response._embedded.venues[0].id;
     searchTicket();
   })
 
 }
 // --- ticketMaster
+
 
 //arrestRecord
 function arrestRecord() {
@@ -232,53 +491,6 @@ function arrestRecord() {
   });
 }
 
-arrestRecord();
-// arrestRecord
-
-//function to search player name
-function runPlayerQuery(queryURL) {
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-
-    playerSearch = "Tom"
-
-    var playerPath = response.players
-
-    for (var i = 0; i < playerPath.length; i++) {
-      
-      if (playerPath[i].firstName == playerSearch) {
-        console.log(playerPath[i])
-        var teamAbbr = playerPath[i].teamAbbr;
-        var playerID = playerPath[i].id;
-        console.log(teamAbbr);
-        console.log(playerID);
-      } else {
-        console.log("nothing found");
-      } 
-    }
-
-    var newsQueryURL = "http://api.fantasy.nfl.com/v1/players/details?playerId=" + playerID + "&statType=seasonStatsformat=json"
-    runNewsQuery(newsQueryURL);
-  })
-
-}
-
-
-//function to pull player news
-function runNewsQuery(queryURL, id) {
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
-    // for (var i = 0; i < playerNews.length; i++) {
-    //   console.log("Player News " + playerNews[i].analysis);
-    // }
-  })
-
-}
 
 
 
@@ -294,9 +506,3 @@ var config = {
 firebase.initializeApp(config);
 
 var gameData = firebase.database().ref("/games");
-
-//Main program
-$( document ).ready(function() {
-  gameScheduleQuery(playerTeam);
-});
-
